80837802e109bb41c902286a6893c804083c902f
Ashley Beal <ashleybeal86@yahoo.com>
2022-06-30 21:33:37 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to an Awesome App about Breads!");
@@ -26,3 +34,6 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+
+
