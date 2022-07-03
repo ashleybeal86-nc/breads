d5715d59fe7a985b1546f8cbf32fd915578f861f
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-03 10:23:52 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,6 +13,17 @@ breads.get('/:arrayIndex', (req, res) => {
   }
 })
 
+breads.get("/:arrayIndex", (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.render("404");
+  }
+});
+
+
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
