ae189d6a76cb4006fc54e5d68ebc0baf58f1b2fc
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-07 14:21:05 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,6 +19,13 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
 
 
 // SHOW
@@ -47,6 +54,17 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 });
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -18,6 +18,9 @@ function Show({ bread, index }) {
       </li>
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
+        <a href={`/breads/${index}/edit`}>
+          <button>Edit</button>
+        </a>
       </form>
     </Default>
   );} 
