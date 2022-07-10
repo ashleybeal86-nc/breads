772e35414b9b44140975599fec5e71f1a813d164
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-10 10:10:06 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 // DEPENDENCIES
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
 
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())// MIDDLEWARE
+app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
