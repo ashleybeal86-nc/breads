e0f7c732cff34fbfc957b1c796c4ce268e0b293f
Ashley Beal <ashleybeal86@yahoo.com>
2022-07-21 20:52:15 -0400

    working on breadcrud

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,7 +7,7 @@ const Baker = require('../models/baker.js');
 // INDEX
 breads.get('/', async (req, res) => {
   const foundBakers = await Baker.find().lean()
-  const foundBreads = await Bread.find().limit(2).lean()
+  const foundBreads = await Bread.find().limit(6).lean()
           res.render('index', {
               breads: foundBreads,
               bakers: foundBakers,
